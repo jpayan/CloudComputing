b9fb49019a33c31f4606b69fcad8dd6e24c74e8b
Unknown <jalexpayan@gmail.com>
2017-08-19 20:22:42 -0700

    Uploaded Yeoman Angular Project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name a1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the a1App
+ */
+angular.module('a1App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
