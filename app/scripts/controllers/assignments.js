4a96a00cdfdd0f85c5873af862eac0806c50f652
Unknown <jalexpayan@gmail.com>
2017-08-22 21:44:26 -0700

    Fixed Assignments Controller and Added Chart

--- app/scripts/controllers/assignments.js
+++ app/scripts/controllers/assignments.js
@@ -8,10 +8,20 @@
  * Controller of the a1App
  */
 angular.module('a1App')
-  .controller('AssignmentsCtrl', function () {
+  .controller('AssignmentsCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  $scope.myGrades = {
+    x: ['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','E1','E2'],
+    y: [94, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    type: 'scatter'
+  };
+
+  var dataSet = [$scope.myGrades];
+  Plotly.newPlot('performanceChart', dataSet);
+
   });
